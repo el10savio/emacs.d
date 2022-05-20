b84da082dfdd1d1b43f61bb37d1051bab8beb2b2
Elton SV <el10savio96@gmail.com>
2022-05-20 23:19:01 +0200

    Update init.el

--- init.el
+++ init.el
@@ -1,3 +1,11 @@
+;; Make startup faster by reducing the frequency of garbage
+;; collection.  The default is 800 kilobytes.  Measured in bytes.
+(setq gc-cons-threshold (* 50 1000 1000))
+
+;; The rest of the init file.
+
+;; Make gc pauses faster by decreasing the threshold.
+(setq gc-cons-threshold (* 2 1000 1000))
 
 ;; Use y/n instead of yes/no
 (fset 'yes-or-no-p 'y-or-n-p)
@@ -8,21 +16,12 @@
 (xclip-mode 1)
 
 ;; Add MELPA
+(require 'use-package)
 (require 'package)
 (add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/") t)
+						 '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
-(defun require-package (package)
-  "Install given PACKAGE if it was not installed before."
-  (if (package-installed-p package)
-      t
-    (progn
-      (unless (assoc package package-archive-contents)
-	(package-refresh-contents))
-      (package-install package))))
-
-;; (package-refresh-contents)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -35,8 +34,9 @@
  '(helm-completion-style 'emacs)
  '(line-number-mode nil)
  '(package-selected-packages
-	 '(rustic evil-org evil-tutor evil xclip cuda-mode erlang xcscope origami flycheck-inline speed-type engine-mode cheat-sh hideshow-org godoctor dired-subtree fancy-dabbrev bats-mode insert-shebang lispy dockerfile-mode gitignore-mode peek-mode load-theme-buffer-local gh-md grip-mode nord-theme fira-code-mode duplicate-thing term-run org-ac smartparens writegood-mode howdoyou howdoi windresize bm yaml-mode ecb go-imenu imenu-list peep-dired magit-delta shell-pop vimish-fold rg sr-speedbar kaolin-themes markdown-mode markdown-mode+ ace-jump-buffer vc-msg git-lens company-go exec-path-from-shell go-imports autopair go-autocomplete go-complete go-mode transpose-frame mood-line marginalia dired-filter dashboard multiple-cursors helm-ag ag perspective diff-hl treemacs which-key git-gutter doom-themes yasnippet-classic-snippets yasnippet-snippets company-lsp lsp-ui lsp-mode magit all-the-icons helm-rg helm-projectile helm golden-ratio company))
- '(recentf-mode t))
+	 '(rustic evil-tutor evil xclip xcscope origami flycheck-inline speed-type engine-mode cheat-sh hideshow-org godoctor dired-subtree fancy-dabbrev bats-mode insert-shebang lispy dockerfile-mode gitignore-mode peek-mode load-theme-buffer-local gh-md grip-mode nord-theme fira-code-mode duplicate-thing term-run org-ac smartparens writegood-mode howdoyou howdoi windresize bm yaml-mode ecb go-imenu imenu-list magit-delta shell-pop vimish-fold rg sr-speedbar kaolin-themes markdown-mode markdown-mode+ ace-jump-buffer vc-msg git-lens company-go exec-path-from-shell go-imports autopair go-autocomplete go-complete go-mode transpose-frame mood-line marginalia dired-filter dashboard multiple-cursors helm-ag ag perspective diff-hl treemacs which-key git-gutter doom-themes yasnippet-classic-snippets yasnippet-snippets company-lsp lsp-ui lsp-mode magit all-the-icons helm-rg helm-projectile helm company))
+ '(recentf-mode t)
+ '(warning-suppress-types '((use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -53,9 +53,9 @@
 ;; quickly open up init.el in new window
 ;;;###autoload
 (defun find-user-init-file ()
-  "Edit the `user-init-file', in another window."
-  (interactive)
-  (find-file-other-window user-init-file))
+	"Edit the `user-init-file', in another window."
+	(interactive)
+	(find-file-other-window user-init-file))
 
 ;; Bind C-c i to open init file in new window
 (global-set-key (kbd "C-c i") #'find-user-init-file)
@@ -78,8 +78,8 @@
 ;; Bind C-/ to comment-or-uncomment-region
 (global-set-key (kbd "C-/") #'comment-or-uncomment-region)
 
-;; Bind C-c s to Global search 
-;; (global-set-key (kbd "C-c s") #'rgrep)
+;; Bind C-c s to Global search
+(global-set-key (kbd "C-c s") #'rgrep)
 
 ;; Local search bind to C-c o
 (global-set-key (kbd "C-c o") #'helm-occur)
@@ -98,27 +98,24 @@
 ;; Display Settings
 ;;
 
-(require-package 'doom-themes)
-(require-package 'golden-ratio)
-
-(require 'golden-ratio)
+(use-package 'doom-themes)
 
 (setq inhibit-startup-message t)
 (menu-bar-mode -1)
 (when (fboundp 'tool-bar-mode)
-  (tool-bar-mode -1))
+	(tool-bar-mode -1))
 (when (fboundp 'scroll-bar-mode)
-  (scroll-bar-mode -1))
+	(scroll-bar-mode -1))
 
 (set-face-attribute 'default nil :height 200)
 (setq-default line-spacing 0.0)
 
 (setq
-      x-select-enable-clipboard t
-      x-select-enable-primary t
-      save-interprogram-paste-before-kill t
-      apropos-do-all t
-      mouse-yank-at-point t)
+			x-select-enable-clipboard t
+			x-select-enable-primary t
+			save-interprogram-paste-before-kill t
+			apropos-do-all t
+			mouse-yank-at-point t)
 
 (setq-default truncate-lines t)
 
@@ -145,10 +142,10 @@
 
 ;; Set default font
 (set-face-attribute 'default nil
-                    :family "Fira Code Medium"
+										:family "Fira Code Medium"
 										:height 120
-                    :weight 'normal
-                    :width 'normal)
+										:weight 'normal
+										:width 'normal)
 
 ;; Default tab size
 (setq default-tab-width 1)
@@ -158,152 +155,122 @@
 ;; modeline
 ;;
 
-(require 'mood-line)
-(mood-line-mode)
-
-(setq-default header-line-format mode-line-format)
-(setq-default mode-line-format nil)
-
-(set-face-background 'header-line "#393c49")
-
-(blink-cursor-mode 1)
-(setq-default cursor-type 'bar)
-(set-cursor-color "#cccccc")
-(setq ring-bell-function 'ignore)
-
-(golden-ratio-mode 1)
+(use-package mood-line
+	:defer t
+	:init
+	(mood-line-mode)
+	(setq-default header-line-format mode-line-format)
+	(setq-default mode-line-format nil)
+	(set-face-background 'header-line "#393c49")
+	(blink-cursor-mode 1)
+	(setq-default cursor-type 'bar)
+	(set-cursor-color "#cccccc")
+	(setq ring-bell-function 'ignore)
+)
 
 ;;
 ;; Dashboard
 ;;
 
-(require 'dashboard)
-(dashboard-setup-startup-hook)
-
-;; Set the title
-(setq dashboard-init-info "Hi Elton, Welcome back to Emacs")
-
-;; Set the banner
-(setq dashboard-startup-banner 'logo)
-;; Value can be
-;; 'official which displays the official emacs logo
-;; 'logo which displays an alternative emacs logo
-;; 1, 2 or 3 which displays one of the text banners
-;; "path/to/your/image.png" or "path/to/your/text.txt" which displays whatever image/text you would prefer
-
-(setq dashboard-items '((recents  . 8)))
-
-(setq dashboard-set-heading-icons t)
-(setq dashboard-set-file-icons t)
-
-(setq dashboard-set-navigator t)
-
-;; Format: "(icon title help action face prefix suffix)"
-;; (setq dashboard-navigator-buttons
-;;       `(;; line1
-;;         ((,(all-the-icons-faicon "server" :height 1.1 :v-adjust 0.0)
-;;          "GoDCApp"
-;;          "Open GoDCApp Repo"
-;;          (lambda (&rest _) (find-file "/Users/evincent/EN/efa-branch/GoDCApp")))
-;;          (,(all-the-icons-faicon "wifi" :height 1.1 :v-adjust 0.0)
-;;          "networking-extreme"
-;;          "Open OS Plugin Repo"
-;;          (lambda (&rest _) (find-file "/Users/evincent/EN/efa-branch/networking-extreme")))
-;;          (,(all-the-icons-faicon "check-circle" :height 1.1 :v-adjust 0.0)
-;;          "todo.org"
-;;          "Open todo.org"
-;;          (lambda (&rest _) (find-file "/Users/evincent/todo.org")))
-;;          (,(all-the-icons-faicon "desktop" :height 1.1 :v-adjust 0.0)
-;;          "emacs.d"
-;;          "Open Emacs Config"
-;;          (lambda (&rest _) (find-file "/home/elton/.emacs.d")))
-;;          )))
-;; 
+(use-package dashboard
+	:defer t
+	:init
+	(dashboard-setup-startup-hook)
+  (setq dashboard-init-info "Hi Elton, Welcome back to Emacs")
+	(setq dashboard-startup-banner 'logo)
+	(setq dashboard-items '((recents  . 8)))
+	(setq dashboard-set-heading-icons t)
+	(setq dashboard-set-file-icons t)
+	(setq dashboard-set-navigator t)
+)
 
 ;; Enable rainbow delimiters
-(require 'rainbow-delimiters)
-(rainbow-delimiters-mode 1)
-
-(show-paren-mode 1)
+(use-package 'rainbow-delimiters
+	:defer t
+	:init
+	(rainbow-delimiters-mode 1)
+	(show-paren-mode 1)
+)
 
 ;;
 ;; git-gutter
 ;;
 (use-package git-gutter
-    :custom
-    (git-gutter:modified-sign "~")
-    (git-gutter:added-sign    "+")
-    (git-gutter:deleted-sign  "-")
-    :custom-face
-    (git-gutter:modified ((t (:background "#f1fa8c"))))
-    (git-gutter:added    ((t (:background "#50fa7b"))))
-    (git-gutter:deleted  ((t (:background "#ff79c6"))))
-    :config
-    (global-git-gutter-mode +1))
+		:defer t
+		:custom
+		(git-gutter:modified-sign "~")
+		(git-gutter:added-sign    "+")
+		(git-gutter:deleted-sign  "-")
+		:custom-face
+		(git-gutter:modified ((t (:background "#f1fa8c"))))
+		(git-gutter:added    ((t (:background "#50fa7b"))))
+		(git-gutter:deleted  ((t (:background "#ff79c6"))))
+		:config
+		(global-git-gutter-mode +1))
 
 ;;
 ;; Duplicate Line/Region
 ;;
 
 (defun duplicate-line()
-  (interactive)
-  (move-beginning-of-line 1)
-  (kill-line)
-  (yank)
-  (open-line 1)
-  (next-line 1)
-  (yank)
+	(interactive)
+	(move-beginning-of-line 1)
+	(kill-line)
+	(yank)
+	(open-line 1)
+	(next-line 1)
+	(yank)
 )
 
 ;; Bind Duplicate Line to C-d
 (global-set-key (kbd "C-d") 'duplicate-line)
 
 (defun duplicate-region-above()
-  "Duplicate the selected region above the cursor."
-  (interactive) (duplicate-region "above"))
+	"Duplicate the selected region above the cursor."
+	(interactive) (duplicate-region "above"))
 
 (defun duplicate-region-below()
-  "Duplicate the selected region below the cursor."
-  (interactive) (duplicate-region "below"))
+	"Duplicate the selected region below the cursor."
+	(interactive) (duplicate-region "below"))
 
 (defun duplicate-region(direction)
-  "Perform the region duplication based on the direction given."
-  (duplicate-region-extend-region-to-line-boundaries)
-  (let
-      ((p1 (region-beginning))
-       (p2 (region-end)))
-    (copy-region-as-kill p1 p2)
-    (goto-char (region-end))
-    (newline)
-    (yank)
-    (duplicate-region-highlight-region direction p1 p2)))
+	"Perform the region duplication based on the direction given."
+	(duplicate-region-extend-region-to-line-boundaries)
+	(let
+			((p1 (region-beginning))
+			 (p2 (region-end)))
+		(copy-region-as-kill p1 p2)
+		(goto-char (region-end))
+		(newline)
+		(yank)
+		(duplicate-region-highlight-region direction p1 p2)))
 
 (defun duplicate-region-highlight-region (direction p1 p2)
-  "Based on DIRECTION, activate the intended region at original P1 and P2."
-  (let
-      ((region-length (- p2 p1)))
-    (cond
-     ((equal direction "above")
-      (goto-char p1)
-      (push-mark p2))
-     ((equal direction "below")
-      (goto-char (+ p2 1))
-      (push-mark (+ (point) (+ region-length 1))))))
+	"Based on DIRECTION, activate the intended region at original P1 and P2."
+	(let
+			((region-length (- p2 p1)))
+		(cond
+		 ((equal direction "above")
+			(goto-char p1)
+			(push-mark p2))
+		 ((equal direction "below")
+			(goto-char (+ p2 1))
+			(push-mark (+ (point) (+ region-length 1))))))
 (setq deactivate-mark nil))
 
 (defun duplicate-region-extend-region-to-line-boundaries ()
-  "Given a selection or line, activate a region that extends to the beginning & end of each line in the region."
-  (let
-      ((p1 (region-beginning))
-       (p2 (region-end)))
-    (if mark-active
-        (progn
-          (goto-char p1)
-          (push-mark (line-beginning-position))
-          (goto-char p2)
-          (goto-char (line-end-position)))
-      (push-mark (line-beginning-position))
-      (goto-char (line-end-position)))))
+	"Given a selection or line, activate a region that extends to the beginning & end of each line in the region."
+	(let
+			((p1 (region-beginning))
+			 (p2 (region-end)))
+		(if mark-active
+				(progn
+					(goto-char p1)
+					(push-mark (line-beginning-position))
+					(goto-char p2)
+					(goto-char (line-end-position)))
+			(push-mark (line-beginning-position))
+			(goto-char (line-end-position)))))
 
 ;;
 ;; Magit
@@ -320,54 +287,32 @@
 ;; (add-hook 'magit-mode-hook (lambda () (magit-delta-mode +1)))
 
 ;;
-;; treemacs
-;;
-;; (use-package treemacs
-;; 	:ensure t
-;; 	:bind
-;; 	(:map global-map
-;; 	      ("C-c t" . treemacs)
-;; 	      ("C-c /" . treemacs-visit-node-vertical-split)
-;; 	      ("C-c -" . treemacs-visit-node-horizontal-split)
-;; 	      )
-;; 	:config
-;; 	(progn
-;; 	  (setq treemacs-is-never-other-window t
-;; 		treemacs-follow-after-init t
-;; 		treemacs-recenter-after-file-follow t
-;; 		treemacs-width 25
-;; 		treemacs-indentation 2
-;; 		treemacs-indentation-string " "
-;;   		treemacs-collapse-dirs (if (executable-find "python") 3 0)
-;; 		treemacs-silent-refresh t
-;; 		treemacs-silent-filewatch t
-;; 		treemacs-sorting 'alphabetic-asc
-;; 		treemacs-indentation 1
-;; 		)))
-;; 
-;; (treemacs-resize-icons 15)
-;; (setq treemacs-follow-mode t)
-;;
-
-;;
 ;; Linum
 ;;
+
 (add-hook 'prog-mode-hook 'linum-mode)
 (setq linum-format "%4d \u2502")
 (set-face-background 'fringe nil)
 
 ;; Highlight current line number
-(require 'hlinum)
-(hlinum-activate)
+(use-package hlinum
+	:defer t
+	:init
+	(hlinum-activate)
+)
+
 
 ;;
 ;; Dired
 ;;
 
-(use-package dired-subtree :ensure t
-  :after dired
-  :config
-  (bind-key "<tab>" #'dired-subtree-cycle dired-mode-map))
+(use-package dired-subtree
+	:defer
+	:ensure t
+	:after dired
+	:config
+	(bind-key "<tab>" #'dired-subtree-cycle dired-mode-map)
+)
 
 ;;
 ;; Marginalia
@@ -375,16 +320,11 @@
 
 ;; Enable richer annotations using the Marginalia package
 (use-package marginalia
-  :init
-
-  (marginalia-mode)
-
-  ;; Prefer richer, more heavy, annotations over the lighter default variant.
-  ;; E.g. M-x will show the documentation string additional to the keybinding.
-  ;; By default only the keybinding is shown as annotation.
-  ;; Note that there is the command `marginalia-cycle-annotators` to
-  ;; switch between the annotators.
-  (setq marginalia-annotators '(marginalia-annotators-heavy marginalia-annotators-light)))
+	:defer t
+	:init
+	(marginalia-mode)
+	(setq marginalia-annotators '(marginalia-annotators-heavy marginalia-annotators-light))
+)
 
 ;;
 ;; Shell
@@ -392,18 +332,18 @@
 
 ;; Open zsh terminal in new window
 (defun term-other-window ()
-  "Open `term` in a new window."
-  (interactive)
-  (let ((buf (term "/bin/zsh")))
-    (switch-to-buffer (other-buffer buf))
-    (switch-to-buffer-other-window buf)))
+	"Open `term` in a new window."
+	(interactive)
+	(let ((buf (term "/bin/zsh")))
+		(switch-to-buffer (other-buffer buf))
+		(switch-to-buffer-other-window buf)))
 
 ;; Open terminal in new window
 ;; and execute watch command
 (defun term-watch-other-window (command)
-  "Open `term` in a new window and execute watch command."
-  (interactive "sWatch: ")
-  (term-run-shell-command (concat "watch " command)))
+	"Open `term` in a new window and execute watch command."
+	(interactive "sWatch: ")
+	(term-run-shell-command (concat "watch " command)))
 
 ;; Bind Open term to C-t
 (global-set-key (kbd "C-t") 'term-other-window)
@@ -420,7 +360,7 @@
 ;;
 ;; YAML Mode
 ;;
-(require 'yaml-mode)
+(use-package 'yaml-mode)
 (add-to-list 'auto-mode-alist '("\\.yaml$" . yaml-mode))
 (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
 
@@ -429,13 +369,13 @@
 ;; Evil Mode
 ;;
 
-;; (require 'evil)
+;; (use-package 'evil)
 ;; (evil-mode 1)
 
 ;;
 ;; HELM
 ;;
-(require 'helm)
+(use-package 'helm)
 
 ;; To make helm-mode start with Emacs init file
 (helm-mode 1)
@@ -445,8 +385,7 @@
 
 ;; Helm Fuzzy Search
 (setq helm-completion-style 'emacs)
-(setq completion-styles `(basic partial-completion emacs22 initials
-                                ,(if (version<= emacs-version "27.0") 'helm-flex 'flex)))
+(setq completion-styles `(basic partial-completion emacs22 initials ,(if (version<= emacs-version "27.0") 'helm-flex 'flex)))
 
 ;; Fix for helm-rg to work
 (setq  helm-rg-ripgrep-executable "/usr/local/bin/rg")
@@ -456,11 +395,15 @@
 ;;
 ;; bm
 ;;
-(require 'bm)
 
-(global-set-key (kbd "C-c b") 'bm-toggle)
-(global-set-key (kbd "C-c ]") 'bm-next)
-(global-set-key (kbd "C-c [") 'bm-previous)
+(use-package bm
+	:defer t
+	:init
+	(global-set-key (kbd "C-c b") 'bm-toggle)
+	(global-set-key (kbd "C-c ]") 'bm-next)
+	(global-set-key (kbd "C-c [") 'bm-previous)
+)
+
 
 ;;
 ;; Avy
@@ -484,7 +427,7 @@
 ;;
 
 ;; Autocomplete org mode
-(require 'org-ac)
+(use-package 'org-ac)
 (org-ac/config-default)
 
 ;;
@@ -492,26 +435,29 @@
 ;;
 
 ;; Enable engine mode
-(require 'engine-mode)
-(engine-mode t)
+(use-package engine-mode
+	:defer t
+	:init
+	(engine-mode t)
+	;; Set firefox as browser
+	;; (setq engine/browser-function 'browse-url-firefox)
 
-;; Set firefox as browser
-;; (setq engine/browser-function 'browse-url-firefox)
+	;; Define duckduckgo Search
+	(defengine duckduckgo
+		"https://duckduckgo.com/?q=%s"
+		:keybinding "d")
 
-;; Define duckduckgo Search 
-(defengine duckduckgo
-  "https://duckduckgo.com/?q=%s"
-  :keybinding "d")
+	;; Bind C-c = to engine mode duckduckgo search
+	(global-set-key (kbd "C-c =") #'engine/search-duckduckgo)
 
-;; Bind C-c = to engine mode duckduckgo search
-(global-set-key (kbd "C-c =") #'engine/search-duckduckgo)
+	;; Set browser
+	(setq browse-url-browser-function 'browse-url-chrome)
+	(setq browse-url-chrome-program "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome")
 
-;; Set browser
-(setq browse-url-browser-function 'browse-url-chrome)
-(setq browse-url-chrome-program "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome")
+	;; Bind M-= to browse-url
+	(global-set-key (kbd "M-=") #'browse-url)
+)
 
-;; Bind M-= to browse-url
-(global-set-key (kbd "M-=") #'browse-url)
 
 ;;
 ;; Code
@@ -521,9 +467,10 @@
 ;; Bash
 ;;
 (use-package lsp-mode
-  :commands lsp
-  :hook
-  (sh-mode . lsp))
+	:defer t
+	:commands lsp
+	:hook
+	(sh-mode . lsp))
 
 ;;
 ;; Go
@@ -537,10 +484,10 @@
 (add-hook 'before-save-hook 'gofmt-before-save)
 
 (defun my-go-mode-hook ()
-  (local-set-key (kbd "M-.") 'godef-jump))
-  (set (make-local-variable 'company-backends) '(company-go))
+	(local-set-key (kbd "M-.") 'godef-jump))
+	(set (make-local-variable 'company-backends) '(company-go))
 
-(require 'cl)
+(use-package 'cl)
 
 (add-hook 'go-mode-hook 'my-go-mode-hook)
 (add-hook 'go-mode-hook 'go-eldoc-setup)
@@ -549,5 +496,19 @@
 (add-hook 'go-mode-hook 'flycheck-mode)
 
 (with-eval-after-load 'flycheck
-  (add-hook 'flycheck-mode-hook #'flycheck-inline-mode))
+	(add-hook 'flycheck-mode-hook #'flycheck-inline-mode))
 (put 'upcase-region 'disabled nil)
+
+
+;;
+;; Startup profiling
+;;
+
+;; Use a hook so the message doesn't get clobbered by other messages.
+(add-hook 'emacs-startup-hook
+ (lambda ()
+  (message "Emacs ready in %s with %d garbage collections."
+  (format "%.2f seconds"
+  (float-time (time-subtract after-init-time before-init-time)))
+  gcs-done))
+)
