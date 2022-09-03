3724f9f94d215f41352e8bd2b7c6ad8a87ee3273
Elton SV <el10savio96@gmail.com>
2022-09-03 13:55:19 +0200

    Create init.el

--- init.el
+++ init.el
@@ -19,6 +19,14 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(eval-and-compile
+  (setq use-package-always-ensure t
+        use-package-expand-minimally t))
+
 (require 'use-package)
 
 (custom-set-variables
